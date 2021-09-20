543d1de00c0f02a57cbbfc40c0a2c9f3ed0ef6d3
Nemezis <maksimkov2000@mail.ru>
2021-09-20 22:55:15 +0300

    Add admin pages

--- js/admin.js
+++ js/admin.js
@@ -3,11 +3,93 @@ function editNews() {
     let date = $(element).find('#date').text();
     let description = $(element).find('#description').text();
     let content = $(element).find('#content').text();
-    $('#form-news').find('#date').val(date);
-    $('#form-news').find('#description').val(description);
-    $('#form-news').find('#content').val(content);
+    $('#form').find('#date').val(date);
+    $('#form').find('#description').val(description);
+    CKEDITOR.instances['content'].setData(content);
 }
 
 function removeNews() {
+    $(this).parent().parent().remove();
+}
+
+function editCategory() {
+    let element = $(this).parent().parent();
+    let category = $(element).find('#category').text();
+    $('#form').find('#category').val(category);
+}
+
+function removeCategory() {
+    $(this).parent().parent().remove();
+}
+
+function editMenu() {
+    let element = $(this).parent().parent();
+    let name = $(element).find('#name').text();
+    let cost = $(element).find('#cost').text();
+    let category = $(element).find('#category').text();
+    $('#form').find('#name').val(name);
+    $('#form').find('#cost').val(cost);
+    $('#form').find('#category').val(category);
+}
+
+function removeMenu() {
+    $(this).parent().parent().remove();
+}
+
+function editRestaurant() {
+    let element = $(this).parent().parent();
+    let name = $(element).find('#name').text();
+    let address = $(element).find('#address').text();
+    $('#form').find('#name').val(name);
+    $('#form').find('#address').val(address);
+}
+
+function removeRestaurant() {
+    $(this).parent().parent().remove();
+}
+
+function removeReview() {
+    $(this).parent().parent().remove();
+}
+
+function editTableOrder() {
+    let element = $(this).parent().parent();
+    let date = $(element).find('#date').text();
+    let restaurant = $(element).find('#restaurant').text();
+    let fio = $(element).find('#fio').text();
+    let guests = $(element).find('#guests').text();
+    let phone = $(element).find('#phone').text();
+    let content = $(element).find('#content').text();
+    $('#form').find('#date').val(date);
+    $('#form').find('#restaurant').val(restaurant);
+    $('#form').find('#fio').val(fio);
+    $('#form').find('#guests').val(guests);
+    $('#form').find('#phone').val(phone);
+    CKEDITOR.instances['content'].setData(content);
+}
+
+function removeTableOrder() {
+    $(this).parent().parent().remove();
+}
+
+function editEventOrder() {
+    let element = $(this).parent().parent();
+    let date = $(element).find('#date').text();
+    let restaurant = $(element).find('#restaurant').text();
+    let fio = $(element).find('#fio').text();
+    let guests = $(element).find('#guests').text();
+    let phone = $(element).find('#phone').text();
+    let content = $(element).find('#content').text();
+    let cost = $(element).find('#cost').text();
+    $('#form').find('#date').val(date);
+    $('#form').find('#restaurant').val(restaurant);
+    $('#form').find('#fio').val(fio);
+    $('#form').find('#guests').val(guests);
+    $('#form').find('#phone').val(phone);
+    $('#form').find('#cost').val(cost);
+    CKEDITOR.instances['content'].setData(content);
+}
+
+function removeEventOrder() {
     $(this).parent().parent().remove();
 }
